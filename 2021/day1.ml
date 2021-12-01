a0b1be5e7200efcafc9e048615512ddf298bcaee
mdano <mage.de.lombre@gmail.com>
2021-12-01 21:21:28 +0100

    Day 1 Ocaml

---
+++ 2021/day1.ml
@@ -0,0 +1,21 @@
+let cnt = 0
+
+let file = open_in "2021/input1.txt"
+let rec get_lines buf f =
+  try
+    let line = input_line file in
+      (int_of_string line)::(get_lines buf f);
+  with e -> close_in_noerr file; buf;;
+
+let nums = get_lines [] file;;
+
+let rec solve1_1 l = match l with
+| hd::hd2::tl-> if hd2 > hd then 1 + (solve1_1 (hd2::tl)) else solve1_1 (hd2::tl)
+| _ -> 0;;
+
+let rec solve1_2 l = match l with
+| hd::hd2::hd3::hd4::tl-> if hd4 > hd then 1 + (solve1_2 (hd2::hd3::hd4::tl)) else solve1_2 (hd2::hd3::hd4::tl)
+| _ -> 0;;
+
+solve1_1 nums;;
+solve1_2 nums;;
