757d90fdbfa4a955947f37057d9ac446b3bc347e
mda01 <mage.de.lombre@gmail.com>
2021-12-02 10:47:23 +0100

    Day 2 python

---
+++ 2021/day2.py
@@ -0,0 +1,39 @@
+f = open("2021/input2.txt")
+lines = list()
+
+for line in f.readlines():
+    d, val = line.split()
+    val = int(val)
+    lines.append((d, val))
+
+
+def solve2_1():
+    dxs = list()
+    dys = list()
+    for direction, v in lines:
+        if direction == 'down':
+            dys.append(v)
+        elif direction == 'up':
+            dys.append(-v)
+        elif direction == 'forward':
+            dxs.append(v)
+    print(sum(dxs) * sum(dys))
+
+
+def solve2_2():
+    aim = 0
+    depth = 0
+    pos = 0
+    for direction, X in lines:
+        if direction == 'down':
+            aim += X
+        elif direction == 'up':
+            aim -= X
+        elif direction == 'forward':
+            pos += X
+            depth += aim * X
+    print(depth * pos)
+
+
+solve2_1()
+solve2_2()
