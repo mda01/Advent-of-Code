d4c3831174ab1cd9eb3c63e46c607bf96ef41d06
mdano <mage.de.lombre@gmail.com>
2021-12-16 20:37:57 +0100

    CPP Reformat (again)

--- 2021/CPP/day1.cpp
+++ 2021/CPP/day1.cpp
@@ -1,41 +1,43 @@
-#include <iostream>
 #include <fstream>
+#include <iostream>
 #include <vector>
 
 using namespace std;
 
 static void solve1_1(ifstream &in) {
-    vector<int> nums;
-    int x;
-    while (in >> x)
-        nums.push_back(x);
-    int cnt = 0;
-    for (int i = 1; i < nums.size(); i++) {
-        if (nums[i] > nums[i - 1]) cnt++;
-    }
-    cout << cnt << endl;
+  vector<int> nums;
+  int x;
+  while (in >> x)
+    nums.push_back(x);
+  int cnt = 0;
+  for (int i = 1; i < nums.size(); i++) {
+    if (nums[i] > nums[i - 1])
+      cnt++;
+  }
+  cout << cnt << endl;
 }
 
 static void solve1_2(ifstream &in) {
-    vector<int> nums;
-    int x;
-    while (in >> x)
-        nums.push_back(x);
-    int cnt = 0;
-    for (int i = 3; i < nums.size(); i++) {
-        if (nums[i] + nums[i - 1] + nums[i - 2] > nums[i - 1] + nums[i - 2] + nums[i - 3]) cnt++;
-    }
-    cout << cnt << endl;
+  vector<int> nums;
+  int x;
+  while (in >> x)
+    nums.push_back(x);
+  int cnt = 0;
+  for (int i = 3; i < nums.size(); i++) {
+    if (nums[i] + nums[i - 1] + nums[i - 2] >
+        nums[i - 1] + nums[i - 2] + nums[i - 3])
+      cnt++;
+  }
+  cout << cnt << endl;
 }
 
-
 int main(int argc, char **argv) {
-    ifstream in;
-    in.open("../input1.txt");
-    solve1_1(in);
-    in.close();
-    in.open("../input1.txt");
-    solve1_2(in);
-    in.close();
-    return 0;
+  ifstream in;
+  in.open("../input1.txt");
+  solve1_1(in);
+  in.close();
+  in.open("../input1.txt");
+  solve1_2(in);
+  in.close();
+  return 0;
 }
--- 2021/CPP/day11.cpp
+++ 2021/CPP/day11.cpp
@@ -1,90 +1,90 @@
-#include <iostream>
 #include <fstream>
+#include <iostream>
 #include <vector>
 
 using namespace std;
 
 int flash(vector<vector<int>> &octos, int x, int y) {
-    int cnt = 1;
-    for (int i = max(x - 1, 0); i <= min(x + 1, (int) octos.size() - 1); i++) {
-        for (int j = max(y - 1, 0); j <= min(y + 1, (int) octos.size() - 1); j++) {
-            octos[i][j]++;
-            if (octos[i][j] == 10) {
-                cnt += flash(octos, i, j);
-            }
-        }
+  int cnt = 1;
+  for (int i = max(x - 1, 0); i <= min(x + 1, (int)octos.size() - 1); i++) {
+    for (int j = max(y - 1, 0); j <= min(y + 1, (int)octos.size() - 1); j++) {
+      octos[i][j]++;
+      if (octos[i][j] == 10) {
+        cnt += flash(octos, i, j);
+      }
     }
-    return cnt;
+  }
+  return cnt;
 }
 
 int updateAndCount(vector<vector<int>> &octos) {
-    int cnt = 0;
-    for (int i = 0; i < octos.size(); i++) {
-        for (int j = 0; j < octos.size(); j++) {
-            octos[i][j]++;
-            if (octos[i][j] == 10) {
-                cnt += flash(octos, i, j);
-            }
-        }
+  int cnt = 0;
+  for (int i = 0; i < octos.size(); i++) {
+    for (int j = 0; j < octos.size(); j++) {
+      octos[i][j]++;
+      if (octos[i][j] == 10) {
+        cnt += flash(octos, i, j);
+      }
     }
-    for (int i = 0; i < octos.size(); i++) {
-        for (int j = 0; j < octos.size(); j++) {
-            if (octos[i][j] >= 10) {
-                octos[i][j] = 0;
-            }
-        }
+  }
+  for (int i = 0; i < octos.size(); i++) {
+    for (int j = 0; j < octos.size(); j++) {
+      if (octos[i][j] >= 10) {
+        octos[i][j] = 0;
+      }
     }
-    return cnt;
+  }
+  return cnt;
 }
 
 static void solve11_1(ifstream &in) {
-    vector<vector<int>> octopuses;
-    string s;
-    while (in >> s) {
-        vector<int> line;
-        for (const char &c: s) {
-            line.push_back(c - '0');
-        }
-        octopuses.push_back(line);
+  vector<vector<int>> octopuses;
+  string s;
+  while (in >> s) {
+    vector<int> line;
+    for (const char &c : s) {
+      line.push_back(c - '0');
     }
-    int cnt = 0;
-    for (int i = 0; i < 100; i++) {
-        cnt += updateAndCount(octopuses);
-    }
-    cout << cnt << endl;
+    octopuses.push_back(line);
+  }
+  int cnt = 0;
+  for (int i = 0; i < 100; i++) {
+    cnt += updateAndCount(octopuses);
+  }
+  cout << cnt << endl;
 }
 
 static void solve11_2(ifstream &in) {
-    vector<vector<int>> octopuses;
-    string s;
-    while (in >> s) {
-        vector<int> line;
-        for (const char &c: s) {
-            line.push_back(c - '0');
-        }
-        octopuses.push_back(line);
+  vector<vector<int>> octopuses;
+  string s;
+  while (in >> s) {
+    vector<int> line;
+    for (const char &c : s) {
+      line.push_back(c - '0');
     }
-    int i = 0;
-    bool allFlash = false;
-    while (!allFlash) {
-        allFlash = true;
-        updateAndCount(octopuses);
-        for (auto &l: octopuses)
-            for (auto &x: l)
-                if (x != 0) allFlash = false;
-        i++;
-    }
-    cout << i << endl;
+    octopuses.push_back(line);
+  }
+  int i = 0;
+  bool allFlash = false;
+  while (!allFlash) {
+    allFlash = true;
+    updateAndCount(octopuses);
+    for (auto &l : octopuses)
+      for (auto &x : l)
+        if (x != 0)
+          allFlash = false;
+    i++;
+  }
+  cout << i << endl;
 }
 
-
 int main(int argc, char **argv) {
-    ifstream in;
-    in.open("../input11.txt");
-    solve11_1(in);
-    in.close();
-    in.open("../input11.txt");
-    solve11_2(in);
-    in.close();
-    return 0;
+  ifstream in;
+  in.open("../input11.txt");
+  solve11_1(in);
+  in.close();
+  in.open("../input11.txt");
+  solve11_2(in);
+  in.close();
+  return 0;
 }
